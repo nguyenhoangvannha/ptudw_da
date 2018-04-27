f9deff040a22c424c3075b968b27c719bb63b944
nguyenhoangvannha <1512363@student.hcmus.edu.vn>
2018-04-27 15:05:23 +0700

    nothing

--- js/search.js
+++ js/search.js
@@ -1,3 +1,15 @@
+function truncateText(selector, maxLength) {
+    var element = document.querySelector(selector),
+        truncated = element.innerText;
+
+    if (truncated.length > maxLength) {
+        truncated = truncated.substr(0,maxLength) + '...';
+    }
+    return truncated;
+}
+
+document.querySelector('p').innerText = truncateText('p', 200);
+
 function createResultItem(name, image, price, des, year) {
     var html = `<!-- Product item -->
     <div class="card flex-md-row mb-4 box-shadow h-md-250">
@@ -7,14 +19,13 @@ function createResultItem(name, image, price, des, year) {
         <a class="text-dark" href="#">$`+price+`</a>
       </h3>
       <div class="mb-1 text-muted">`+year+`</div>
-      <textarea class="card-text mb-auto" cols="50" maxlength="50">`+des+`</textarea>
+      <p style="width: 400px;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;">`+des+`</p>
       <a href="product.html">read more</a>
     </div>
-    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="`+name+`" style="width: 250px; height: 200px;" src="`+image+`" data-holder-rendered="true">
+    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="`+name+`" style="width: 220px; height: 170px;" src="`+image+`" data-holder-rendered="true">
 
     <div class="product_extras">
-                        
-                        <button class="product_cart_button">Thêm vào giỏ hàng</button>
+                        <a href="javascript:void(0);" name="" id="" class="btn btn-primary btn-buy" style="color: white" role="button">Thêm vào giỏ hàng</a>
                     </div>
   </div>`
   return html;
@@ -48,3 +59,35 @@ function createResultContent(jsonFile, callback) {
         callback(html);
     });
 }
+
+$(document).on('click','.btn-buy',function (e) {
+    e.preventDefault();
+    var parent = $(this).parents('.card' ,'.flex-md-row', '.mb-4 box-shadow', '.h-md-250');
+    var cart = $(document).find('#cart-shop');
+        src = parent.find('img').attr('src');
+        
+        var parTop = parent.offset().top;
+        var parLeft = parent.offset().left;
+    
+        $('<img />', { 
+          class: 'img-fly',
+          src: src,
+        }).appendTo('body').css({
+            'top' : parTop,
+            'left' : parseInt(parLeft) + parseInt(parent.width()) - 50
+        });
+
+    setTimeout(function(){
+        $(document).find('.img-fly').css({
+        'top' : cart.offset().top,
+        'left' : cart.offset().left
+        });
+        setTimeout(function(){
+            $(document).find('.img-fly').remove();
+            var citem = parseInt(cart.find('#count-item').data('count'))+1;
+            cart.find('#count-item').text(citem).data('count',citem);
+        },1000);	
+    },500);
+});
+
+
